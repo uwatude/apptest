272f090cc7b7bfabc4b416f89441e0ac48e8089c
uwatude <113748435+uwatude@users.noreply.github.com>
2022-09-21 00:26:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from tkinter import Y
+
 from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
 from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
